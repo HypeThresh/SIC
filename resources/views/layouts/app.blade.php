e9ed69992f9dfa6fec41737c08c43e8cedec645c
Hype Thresh <hypethresh@gmail.com>
2021-11-28 14:17:31 -0600

    rename

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
     @auth {{-- si el usuario esta auntenticado --}}
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow">
         <div class="container-fluid">
-            <a class="navbar-brand" href="/home">Hotel San Francisco</a>
+            <a class="navbar-brand" href="/home">Comercial El Águila</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
